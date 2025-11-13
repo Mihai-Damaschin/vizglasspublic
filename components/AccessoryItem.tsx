894503e8ad4a4bd70b1814de91088c5fba34bd8e
Dmitri Ganenco <dmitri.ganenco@ebs-integrator.com>
2025-11-14 00:26:50 +0200

    wip: update styles for accessories and glasses pages

--- components/AccessoryItem.tsx
+++ components/AccessoryItem.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { theme } from "antd";
-import { useStyleRegister } from "@ant-design/cssinjs";
-
-const { useToken } = theme;
 
 interface AccessoryItemProps {
   name: string;
@@ -20,102 +16,53 @@ const AccessoryItem = ({
   imageUrl,
   isEven = false,
   showDivider = false,
-}: AccessoryItemProps) => {
-  const { token, theme } = useToken();
-
-  const wrapSSR = useStyleRegister(
-    { theme, token, path: ["AccessoryItem"] },
-    () => ({
-      ".accessory-item-container": {
+}: AccessoryItemProps) => (
+  <div>
+    <div
+      style={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         gap: "5rem",
-
-        "@media (max-width: 767px)": {
-          gap: "2rem",
-          flexDirection: "column",
-        },
-      },
-
-      ".accessory-item-row": {
-        flexDirection: "row",
-        "@media (max-width: 767px)": {
-          flexDirection: "column",
-        },
-      },
-
-      ".accessory-item-row-reverse": {
-        flexDirection: "row-reverse",
-        "@media (max-width: 767px)": {
-          flexDirection: "column",
-        },
-      },
-
-      ".accessory-item-text": {
-        width: 500,
-        "@media (max-width: 991px)": {
-          width: 300,
-        },
-        "@media (max-width: 767px)": {
-          width: "100%",
-        },
-      },
-
-      ".accessory-item-title": {
-        fontSize: "18px",
-        marginBottom: "0.5rem",
-      },
-
-      ".accessory-item-description": {
-        fontSize: "14px",
-        textAlign: "justify",
-        opacity: 0.6,
-        whiteSpace: "pre-line",
-      },
-
-      ".accessory-item-divider": {
-        height: 1,
-        backgroundColor: "gray",
-        opacity: 0.2,
-        margin: "5rem",
-      },
-
-      ".accessory-item-image": {
-        borderRadius: "1rem",
-
-        "@media (max-width: 576px)": {
-          width: "300px !important",
-          height: "300px !important",
-        },
-      },
-    }),
-  );
-
-  return wrapSSR(
-    <div>
-      <div
-        className={`accessory-item-container ${
-          isEven ? "accessory-item-row" : "accessory-item-row-reverse"
-        }`}
-      >
-        <div className="accessory-item-text">
-          <h3 className="accessory-item-title">{name}</h3>
-          <p className="accessory-item-description">{description}</p>
-        </div>
-
-        <Image
-          src={imageUrl}
-          alt={name}
-          width={400}
-          height={400}
-          className="accessory-item-image"
-        />
+        flexDirection: isEven ? "row" : "row-reverse",
+      }}
+      className="accessory-box"
+    >
+      <div className="accessory-text-box">
+        <h3 style={{ fontSize: "18px", marginBottom: "0.5rem" }}>{name}</h3>
+        <p
+          style={{
+            fontSize: "14px",
+            textAlign: "justify",
+            opacity: 0.6,
+            whiteSpace: "pre-line",
+          }}
+        >
+          {description}
+        </p>
       </div>
 
-      {showDivider && <div className="accessory-item-divider" />}
-    </div>,
-  );
-};
+      <Image
+        src={imageUrl}
+        alt={name}
+        width={400}
+        height={400}
+        style={{ borderRadius: "1rem" }}
+        className="accessory-image"
+      />
+    </div>
+
+    {showDivider && (
+      <div
+        style={{
+          height: 1,
+          backgroundColor: "gray",
+          opacity: 0.2,
+          margin: "5rem",
+        }}
+      />
+    )}
+  </div>
+);
 
 export default AccessoryItem;
